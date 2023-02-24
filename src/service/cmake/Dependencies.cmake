8fdf490b61314b0de06163b605e3ec156fc3923b
Alexander Krimm <alex@wirew0rm.de>
2023-02-24 15:23:22 +0100

    AcquisitionWorker: add unittest

--- src/service/cmake/Dependencies.cmake
+++ src/service/cmake/Dependencies.cmake
@@ -6,4 +6,10 @@ FetchContent_Declare(
         GIT_TAG main # todo: use proper release once available
 )
 
-FetchContent_MakeAvailable(opencmw-cpp)
+FetchContent_Declare(
+        ut
+        GIT_REPOSITORY https://github.com/boost-ext/ut.git
+        GIT_TAG 265199e173b16a75670fae62fc2446b9dffad39e # head as of 2022-12-19
+)
+
+FetchContent_MakeAvailable(opencmw-cpp ut)
