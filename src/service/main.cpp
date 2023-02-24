4beb1c3cd5c4860a091d3f0847e9d42246e266da
Alexander Krimm <alex@wirew0rm.de>
2023-02-24 15:18:02 +0100

    implement Acquisition Worker

--- src/service/main.cpp
+++ src/service/main.cpp
@@ -1,8 +1,8 @@
+#include <Client.hpp>
 #include <majordomo/Broker.hpp>
 #include <majordomo/Worker.hpp>
 
 #include <fstream>
-#include <iomanip>
 #include <thread>
 
 #include "acquisition/acqWorker.hpp"
@@ -13,6 +13,10 @@ using namespace opencmw::majordomo;
 
 int main() {
     using opencmw::URI;
+    using namespace opendigitizer::acq;
+    using namespace opencmw::majordomo;
+    using namespace std::chrono_literals;
+
     // broker
     Broker broker("PrimaryBroker");
     // REST backend
@@ -31,16 +35,45 @@ int main() {
     });
 
     // flowgraph worker (mock)
-    FlowgraphWorker<"flowgraph", description<"Provides R/W access to the flowgraph as a yaml serialized string">> flowgraphWorker(broker);
-    std::jthread                                                                                                  flowgraphWorkerThread([&flowgraphWorker] {
-        flowgraphWorker.run();
-                                                                                                     });
+    using FgWorker = FlowgraphWorker<"flowgraph", description<"Provides R/W access to the flowgraph as a yaml serialized string">>;
+    FgWorker     flowgraphWorker(broker);
+    std::jthread flowgraphWorkerThread([&flowgraphWorker] { flowgraphWorker.run(); });
 
-    // acquisition worker (mock) todo: implement
-    AcquisitionWorker<"acquisition", description<"Provides data acquisition updates">> acquisitionWorker(broker, 2000ms); // todo: change to 25Hz, just slow for debugging
-    std::jthread                                                                       acquisitionWorkerThread([&acquisitionWorker] {
-        acquisitionWorker.run();
-                                                                          });
+    // acquisition worker (mock)
+    using AcqWorker = AcquisitionWorker<"/DeviceName/Acquisition", description<"Provides data acquisition updates">>;
+    AcqWorker    acquisitionWorker(broker, 3000ms);
+    std::jthread acquisitionWorkerThread([&acquisitionWorker] { acquisitionWorker.run(); });
+
+    std::this_thread::sleep_for(100ms);
+
+    // start some simple subscription client
+    fmt::print("starting some client subscriptions\n");
+    const Context                                             zctx{};
+    std::vector<std::unique_ptr<opencmw::client::ClientBase>> clients;
+    clients.emplace_back(std::make_unique<opencmw::client::MDClientCtx>(zctx, 20ms, ""));
+    opencmw::client::ClientContext client{ std::move(clients) };
+
+    std::this_thread::sleep_for(100ms);
+
+    std::atomic<int> receivedA{ 0 };
+    std::atomic<int> receivedAB{ 0 };
+    client.subscribe(URI("mds://127.0.0.1:12345/DeviceName/Acquisition?channelNameFilter=saw"), [&receivedA](const opencmw::mdp::Message &update) {
+        fmt::print("Client('saw') received message from service '{}' for endpoint '{}'\n{}\n", update.serviceName.str(), update.endpoint.str(), update.data.asString());
+        receivedA++;
+    });
+    client.subscribe(URI("mds://127.0.0.1:12345/DeviceName/Acquisition"), [&receivedAB](const opencmw::mdp::Message &update) {
+        fmt::print("Client('all') received message from service '{}' for endpoint '{}'\n{}\n", update.serviceName.str(), update.endpoint.str(), update.data.asString());
+        receivedAB++;
+    });
+
+    while (receivedA < 2 || receivedAB < 4) {
+        std::this_thread::sleep_for(200ms);
+    }
+
+    client.unsubscribe(URI("mds://127.0.0.1:12345/DeviceName/Acquisition?channelNameFilter=saw"));
+    client.unsubscribe(URI("mds://127.0.0.1:12345/DeviceName/Acquisition"));
+    fmt::print("received client updates: {} for 'sine' and {} for 'sine,saw'\n", receivedA, receivedAB);
+    client.stop();
 
     // shutdown
     brokerThread.join();
