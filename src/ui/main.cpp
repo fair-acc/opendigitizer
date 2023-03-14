ab01220e39fe61c9e9c8c34fb6f7c0b3b603d561
Alexander Krimm <A.Krimm@gsi.de>
2023-03-14 16:58:45 +0100

    disable gamepad support which fails for wasm files loaded over plain http

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -177,7 +177,7 @@ struct App {
 
 int main(int, char **) {
     // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
         printf("Error: %s\n", SDL_GetError());
         return -1;
     }
