6852a2715ca2ae95abeed27116beeb7ffc17e95c
Frank Osterfeld <frank.osterfeld@kdab.com>
2023-11-09 16:10:15 +0100

    Update gr-digitizers/graph-prototype

--- src/service/cmake/Dependencies.cmake
+++ src/service/cmake/Dependencies.cmake
@@ -8,10 +8,12 @@ FetchContent_Declare(
         GIT_TAG a245ee17adaa61f530d95883da38b68fb53f4de8
 )
 
+# TODO this should be a graph-prototype dependency only, as gr-digitizer blocks
+# (e.g. PicoScope) would be loaded via plugins, at runtime
 FetchContent_Declare(
         gr-digitizers
         GIT_REPOSITORY https://github.com/fair-acc/gr-digitizers.git
-        GIT_TAG 4bd135940cdf2b82b195fcdac0a662da7bdc8689 # latest dev-prototype as of 2023-11-09
+        GIT_TAG fe402e92b7e4cfc8ffc745f2349ab26b941fc8b9 # latest dev-prototype as of 2023-11-09
 )
 
 FetchContent_Declare(
