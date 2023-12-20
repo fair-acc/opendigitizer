bc2a6a2072bb22d6ae36d81ad93108a10640e188
rstein <r.steinhagen@gsi.de>
2023-12-20 15:41:03 +0100

    added C-API RAII-wrapper
    ... and refactored common cmake dependencies to parent
    
    Signed-off-by: Ralph J. Steinhagen <r.steinhagen@gsi.de>

--- src/service/cmake/Dependencies.cmake
+++ src/service/cmake/Dependencies.cmake
@@ -1,12 +1,5 @@
 include(FetchContent)
 
-# TODO use proper releases once available
-FetchContent_Declare(
-        opencmw-cpp
-        GIT_REPOSITORY https://github.com/fair-acc/opencmw-cpp.git
-        GIT_TAG 57f31a19d8998da944ec73223d7f3fba4feeb324
-)
-
 # TODO this should be a graph-prototype dependency only, as gr-digitizer blocks
 # (e.g. PicoScope) would be loaded via plugins, at runtime
 FetchContent_Declare(
@@ -15,10 +8,4 @@ FetchContent_Declare(
         GIT_TAG fe402e92b7e4cfc8ffc745f2349ab26b941fc8b9 # latest dev-prototype as of 2023-11-09
 )
 
-FetchContent_Declare(
-        ut
-        GIT_REPOSITORY https://github.com/boost-ext/ut.git
-        GIT_TAG v2.0.1 # latest version as of 2023-12-19
-)
-
-FetchContent_MakeAvailable(opencmw-cpp gr-digitizers ut)
+FetchContent_MakeAvailable(gr-digitizers)
