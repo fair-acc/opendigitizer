058600b687badb13f2237d37a72f717f57199a15
Alexander Krimm <alex@wirew0rm.de>
2024-04-24 17:07:37 +0200

    CI: use gnuradio4 docker container

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,15 +1,15 @@
 include(FetchContent)
 
 FetchContent_Declare(
-        opencmw-cpp
-        GIT_REPOSITORY https://github.com/fair-acc/opencmw-cpp.git
-        GIT_TAG c7236c423f4f1f4de8a762c1e946f6ad76cd0a8d # main as of 2024-03-13
+        graph-prototype
+        GIT_REPOSITORY https://github.com/fair-acc/graph-prototype.git
+        GIT_TAG 5e15e8478d267a5e74fdd3b310a7483a690fe1b4 # main as of 2024-04-25
 )
 
 FetchContent_Declare(
-        graph-prototype
-        GIT_REPOSITORY https://github.com/fair-acc/graph-prototype.git
-        GIT_TAG 3cb5c2aff7046bc3760ccd8f8b701cbde361ecfd # main as of 2024-04-05
+        opencmw-cpp
+        GIT_REPOSITORY https://github.com/fair-acc/opencmw-cpp.git
+        GIT_TAG 0fb3758c3ffe7707aa5e0bd2ad25f9e8fb19f79d# main as of 2024-04-26
 )
 
 FetchContent_Declare(
--- src/service/cmake/Dependencies.cmake
+++ src/service/cmake/Dependencies.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
         gr-digitizers
         GIT_REPOSITORY https://github.com/fair-acc/gr-digitizers.git
-        GIT_TAG 417f997f696f2bdaadfaf41ac7d8a339a95481be # dev-prototype as of 2024-04-05
+        GIT_TAG 0ad25d0d2947aac288cf76b1626816a5044b753e # dev-prototype as of 2024-04-25
 )
 
 FetchContent_MakeAvailable(gr-digitizers)
