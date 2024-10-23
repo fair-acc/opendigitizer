90a3ecfa97822f5c57b41d3ab1fb10d2a27592be
Alexander Krimm <A.Krimm@gsi.de>
2024-10-23 10:23:34 +0200

    Picoscope flowgraph: change types and channel settings
    Followup for changes in gr-digitizer which limit the output types of the
    digitizer to float, unint16 and UncertainValue<float>.
    Due to the http subscriptions only working for double values (to be
    investigated why) the values are converted to double using a converter
    block.
    
    Also changes the name of channelA to test, s.t. the same dashboards as
    for the builtin test flowgraph can be used.
    
    Signed-off-by: Alexander Krimm <A.Krimm@gsi.de>

--- src/service/main.cpp
+++ src/service/main.cpp
@@ -15,6 +15,7 @@
 
 // TODO instead of including and registering blocks manually here, rely on the plugin system
 #include <Picoscope4000a.hpp>
+#include <gnuradio-4.0/basic/ConverterBlocks.hpp>
 #include <gnuradio-4.0/basic/Selector.hpp>
 #include <gnuradio-4.0/basic/common_blocks.hpp>
 #include <gnuradio-4.0/basic/function_generator.hpp>
@@ -79,6 +80,7 @@ void registerTestBlocks(Registry& registry) {
 #pragma GCC diagnostic ignored "-Wunused-variable"
     gr::registerBlock<TestSource, double, float>(registry);
     gr::registerBlock<gr::basic::DataSink, double, float, std::int16_t>(registry);
+    gr::registerBlock<gr::blocks::type::converter::Convert, gr::BlockParameters<double, float>, gr::BlockParameters<float, double>>(registry);
     gr::registerBlock<fair::picoscope::Picoscope4000a, fair::picoscope::AcquisitionMode::Streaming, float, std::int16_t>(registry); // ommitting gr::UncertainValue<float> for now, which would also be supported by picoscope block
     fmt::print("providedBlocks:\n");
     for (auto& blockName : registry.providedBlocks()) {
