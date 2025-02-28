3af477cbadb47c39cd6a561a84a89938fc376db9
Sergio Martins <sergio.martins@kdab.com>
2025-02-28 16:42:00 +0000

    Add block navigation in the blocks property panel
    This makes it easy to see only the immediate neighbours.
    
    We have very little space in the properties panel, thus the design
    needs to be a bit different than the main FG:
      - Don't show port name (use tooltip instead)
      - Blocks are smaller
      - Less text shown (just the title)
      - Possibly we'll want word wrap
      - All goes inside an horizontal scroll area
    
    The ports are colored, as suggested. But not sure which types should
    use which colors. Red ports for now.
    
    The styling is separated from the business logic, which is hidden
    by a callback and context object.
    
    For issue #202
    
    Signed-off-by: Sergio Martins <sergio.martins@kdab.com>

---
+++ src/ui/components/BlockNeighborsPreview.hpp
@@ -0,0 +1,16 @@
+#ifndef OPENDIGITIZER_UI_COMPONENTS_BLOCK_NEIGHBORS_PREVIEW_HPP_
+#define OPENDIGITIZER_UI_COMPONENTS_BLOCK_NEIGHBORS_PREVIEW_HPP_
+
+#include <imgui.h>
+
+struct UiGraphBlock;
+
+namespace DigitizerUi::components {
+
+struct BlockControlsPanelContext;
+
+void BlockNeighborsPreview(const BlockControlsPanelContext&, ImVec2 availableSize);
+
+} // namespace DigitizerUi::components
+
+#endif
