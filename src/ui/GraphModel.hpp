178c2844533b641e3b919bf82c93c46e4660a0e3
Ivan Čukić <ivan.cukic@kdab.com>
2025-04-03 10:40:08 +0200

    Use descriptions instead of keys when showing properties
    Signed-off-by: Ivan Čukić <ivan.cukic@kdab.com>

--- src/ui/GraphModel.hpp
+++ src/ui/GraphModel.hpp
@@ -55,6 +55,15 @@ struct UiGraphBlock {
     gr::property_map blockSettings;
     gr::property_map blockMetaInformation;
 
+    struct SettingsMetaInformation {
+        std::string unit;
+        std::string description;
+        bool        isVisible;
+    };
+
+    std::map<std::string, SettingsMetaInformation> blockSettingsMetaInformation;
+    void                                           updateBlockSettingsMetaInformation();
+
     std::vector<UiGraphPort> inputPorts;
     std::vector<UiGraphPort> outputPorts;
 
